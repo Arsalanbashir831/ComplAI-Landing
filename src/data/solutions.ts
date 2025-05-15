da9f05173ef50f1970b870515bc043b66c65d11a
Saadaan Hassan <webmaker9d@gmail.com>
2025-05-16 03:34:56 +0500

    Replace the solution images from svg to png format

--- src/data/solutions.ts
+++ src/data/solutions.ts
@@ -4,7 +4,7 @@ export const solutions: Solution[] = [
   {
     title: 'Companion',
     description: 'Your AI-powered compliance expert.',
-    image: '/images/companion.svg',
+    image: '/images/companion.png',
     buttonLabel: 'Learn More →',
     buttonLink: '/companion',
     imageAlign: 'center',
@@ -12,34 +12,34 @@ export const solutions: Solution[] = [
   {
     title: 'Resolve',
     description: 'Let AI handle complaints efficiently.',
-    image: '/images/resolve.svg',
+    image: '/images/resolve.png',
     buttonLabel: 'Learn More →',
     buttonLink: '/resolve',
   },
   {
     title: 'Compose',
     description: 'Create compliant policies with AI support',
-    image: '/images/compose.svg',
+    image: '/images/compose.png',
     comingSoon: true,
   },
   {
     title: 'Review',
     description:
       'Upload your policies and let AI find and fix compliance gaps.',
-    image: '/images/review.svg',
+    image: '/images/review.png',
     comingSoon: true,
   },
   {
     title: 'Validate',
     description: 'Perform file reviews and identify key compliance themes',
-    image: '/images/validate.svg',
+    image: '/images/validate.png',
     comingSoon: true,
   },
   {
     title: 'Audit',
     description:
       'Achieve audit readiness for external audits such as Lexcel, SRA and Legal Aid.',
-    image: '/images/audit.svg',
+    image: '/images/audit.png',
     comingSoon: true,
   },
 ];
