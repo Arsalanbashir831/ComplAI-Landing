6028b48bd9ca89c82a918e725c74e2d406e12416
Arsalan <arsalanbashir831@gmail.com>
2025-10-28 17:23:33 +0500

    refactor: reorder imports in early access requests page and related services for improved organization and consistency

--- src/app/(landing)/news/layout.tsx
+++ src/app/(landing)/news/layout.tsx
@@ -1,6 +1,6 @@
-import { NEWS_META_DATA } from '@/constants/meta-data';
-import { Metadata } from 'next';
 import { ReactNode } from 'react';
+import { Metadata } from 'next';
+import { NEWS_META_DATA } from '@/constants/meta-data';
 
 interface NewsLayoutProps {
   children: ReactNode;
